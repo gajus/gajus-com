7cfc0e164bafd3356efd8b565dc0fb4da428378f
Gajus Kuizinas <gajus@gajus.com>
2023-10-09 23:39:04 -0600

    wip

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,9 +1,7 @@
 import { sentryConfig } from './sentryConfig';
 import { init } from '@sentry/nextjs';
-import { ProfilingIntegration } from '@sentry/profiling-node';
 
 init({
   ...sentryConfig,
-  integrations: [new ProfilingIntegration()],
   profilesSampleRate: 0.1,
 });
