bf3a02056f4390437de8eb1c093bf509cca774ad
Gajus Kuizinas <gajus@gajus.com>
2023-10-10 09:19:04 -0600

    perf: add cache()

--- src/routines/getBlogPostHeads.ts
+++ src/routines/getBlogPostHeads.ts
@@ -7,55 +7,58 @@ import parseFrontMatter from 'front-matter';
 import { readdir, readFile } from 'node:fs/promises';
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { cache } from 'react';
 
-export const getBlogPostHeads = async (): Promise<readonly BlogPostHead[]> => {
-  const blogPostPaths = await readdir(
-    resolve(dirname(fileURLToPath(import.meta.url)), '../blogPosts'),
-  );
-
-  const blogPosts = [];
-
-  for (const blogPostSlug of blogPostPaths) {
-    const file = await readFile(
-      resolve(
-        dirname(fileURLToPath(import.meta.url)),
-        `../blogPosts`,
-        blogPostSlug,
-        'blogPost.mdx',
-      ),
+export const getBlogPostHeads = cache(
+  async (): Promise<readonly BlogPostHead[]> => {
+    const blogPostPaths = await readdir(
+      resolve(dirname(fileURLToPath(import.meta.url)), '../blogPosts'),
     );
 
-    const { attributes } = parseFrontMatter(file.toString()) as {
-      attributes: {
-        author: string;
-        description: string;
-        guid: string;
-        publishedAt: Date;
-        tags: readonly string[];
-        title: string;
+    const blogPosts = [];
+
+    for (const blogPostSlug of blogPostPaths) {
+      const file = await readFile(
+        resolve(
+          dirname(fileURLToPath(import.meta.url)),
+          `../blogPosts`,
+          blogPostSlug,
+          'blogPost.mdx',
+        ),
+      );
+
+      const { attributes } = parseFrontMatter(file.toString()) as {
+        attributes: {
+          author: string;
+          description: string;
+          guid: string;
+          publishedAt: Date;
+          tags: readonly string[];
+          title: string;
+        };
       };
-    };
 
-    blogPosts.push(
-      BlogPostHeadZodSchema.parse({
-        ...attributes,
-        author: {
-          name: 'Gajus Kuizinas',
-        },
-        publishedAt: attributes.publishedAt
-          ? new Date(attributes.publishedAt)
-          : undefined,
-        slug: blogPostSlug,
-        source: join(blogPostSlug, 'blogPost.mdx'),
-        tags: attributes.tags.map((tag) => getTag(tag)),
-      }),
+      blogPosts.push(
+        BlogPostHeadZodSchema.parse({
+          ...attributes,
+          author: {
+            name: 'Gajus Kuizinas',
+          },
+          publishedAt: attributes.publishedAt
+            ? new Date(attributes.publishedAt)
+            : undefined,
+          slug: blogPostSlug,
+          source: join(blogPostSlug, 'blogPost.mdx'),
+          tags: attributes.tags.map((tag) => getTag(tag)),
+        }),
+      );
+    }
+
+    // eslint-disable-next-line require-atomic-updates
+    return blogPosts.sort(
+      (a, b) =>
+        (b.publishedAt?.getTime() ?? Number.POSITIVE_INFINITY) -
+        (a.publishedAt?.getTime() ?? Number.POSITIVE_INFINITY),
     );
-  }
-
-  // eslint-disable-next-line require-atomic-updates
-  return blogPosts.sort(
-    (a, b) =>
-      (b.publishedAt?.getTime() ?? Number.POSITIVE_INFINITY) -
-      (a.publishedAt?.getTime() ?? Number.POSITIVE_INFINITY),
-  );
-};
+  },
+);
