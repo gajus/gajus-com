b70e27a099dfa3210986419b0da96825f03e2eab
Gajus Kuizinas <gajus@gajus.com>
2023-10-10 10:54:07 -0600

    fix: attempt to force include /src/blogPosts

--- src/routines/getBlogPostBody.ts
+++ src/routines/getBlogPostBody.ts
@@ -1,27 +1,27 @@
 import { bundleMDX } from './bundleMDX';
 import { type BlogPostHead } from '@/zodSchemas/BlogPostHeadZodSchema';
 import { readFile } from 'node:fs/promises';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { resolve } from 'node:path';
+import { cache } from 'react';
 
 type BlogPostBody = {
   code: string;
 };
 
-export const getBlogPostBody = async (
-  blogPostHead: BlogPostHead,
-): Promise<BlogPostBody> => {
-  const blogPostPath = resolve(
-    dirname(fileURLToPath(import.meta.url)),
-    `../blogPosts/`,
-    blogPostHead.source,
-  );
+export const getBlogPostBody = cache(
+  async (blogPostHead: BlogPostHead): Promise<BlogPostBody> => {
+    const blogPostPath = resolve(
+      process.cwd() + '/src/blogPosts',
+      `../blogPosts/`,
+      blogPostHead.source,
+    );
 
-  const blogPostRaw = await readFile(blogPostPath, 'utf8');
+    const blogPostRaw = await readFile(blogPostPath, 'utf8');
 
-  const { code } = await bundleMDX(blogPostRaw);
+    const { code } = await bundleMDX(blogPostRaw);
 
-  return {
-    code,
-  };
-};
+    return {
+      code,
+    };
+  },
+);
