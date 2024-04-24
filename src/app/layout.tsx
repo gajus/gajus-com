58494e4de0f60595897b296847ea7bb95994ebdf
Gajus <gajus@gajus.com>
2024-04-24 11:42:08 -0600

    Revert "wip"
    This reverts commit 96e841f7f27a2cd1c6e1e386d60a695abb482482.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unassigned-import
 import './globals.css';
 import { GoogleAnalytics } from '#app/components/GoogleAnalytics';
-import { type Metadata, type Viewport } from 'next';
+import { type Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Suspense } from 'react';
 
@@ -20,12 +20,11 @@ export const metadata: Metadata = {
   ],
   metadataBase: new URL('https://gajus.com'),
   title: 'Gajus',
-};
-
-export const viewport: Viewport = {
-  initialScale: 1,
-  maximumScale: 1,
-  width: 'device-width',
+  viewport: {
+    initialScale: 1,
+    maximumScale: 1,
+    width: 'device-width',
+  },
 };
 
 export default async ({ children }: { readonly children: React.ReactNode }) => {
